243f6f900676dedee05b15676f5dddc1e1505ca0
suzanece <suzaneceada@gmail.com>
2016-12-19 04:58:24 +0200

    New FitNowApp1 v1.3

--- FitterNowApp1/settings.py
+++ FitterNowApp1/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'is9%+d5ef^i_f)s(d7%r=)bt6#jlkx!svpm8mj*0!-tr3n1*+l'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["http://35.156.191.4/"]
 
 
 # Application definition
