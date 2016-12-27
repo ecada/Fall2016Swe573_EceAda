2088737f97c01f80f42e12191f0e319781c0b340
suzanece <suzaneceada@gmail.com>
2016-12-27 16:13:01 +0200

    Tests

--- fitternow/tests.py
+++ fitternow/tests.py
@@ -16,16 +16,35 @@ from rest_framework.test import APIClient
 
 
 class UserTestCase(TestCase):
-
     def setUp(self):
         self.user = User.objects.create_user(username='milkshake', first_name='joe', last_name='doe',
                                              email='user@fitternow.com', password='pass123')
 
-
     def test_token_created(self):
-
         token = Token.objects.get(user_id=self.user.id)
 
         self.assertIsNotNone(token)
         self.assertIsNotNone(token.key)
         self.assertEqual(self.user.id, token.user_id)
+
+        self.assertEqual(self.user.username, 'milkshake')
+        self.assertEqual(self.user.first_name, 'joe')
+        self.assertEqual(self.user.last_name, 'doe')
+        self.assertEqual(self.user.email, 'user@fitternow.com')
+
+        # self.assertEqual(self.user.password, 'pbkdf2_sha256$30000$3GFPgLi8cNYw$Na5f1jfU/jw7TwhhYaGdrmZU76O2h4zIupnt/7ydvOA')
+        # UserActivities.objects.perform_create(username='milkshake', activity_name='Archery', duration='2',
+        #   calories_burned='300')
+
+
+class ActivityTestCase(TestCase):
+
+    def setUp(self):
+        Activity.objects.create( activity_name='TestActivity', calories=400)
+    def test_activity_created(self):
+        obj = Activity.objects.get(activity_name='TestActivity')
+        self.assertEqual(obj.calories,400)
+
+
+
+
